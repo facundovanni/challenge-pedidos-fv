469d259d6150d489426d70eab503c046082b3c69
facundovanni <facundovanni@gmail.com>
2026-05-27 19:55:55 -0300

    Add shorcut to cart in header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CartService } from '../../../core/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,6 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent { }
+export class HeaderComponent {
+  public cartService = inject(CartService);
+ }
